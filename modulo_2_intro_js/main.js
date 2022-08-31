a9d8e59682f33a714e7b9ad05dab8f262eb5f04e
Daniel <danielsquinteros@gmail.com>
2022-08-30 21:58:29 -0400

    introduccion a js

---
+++ modulo_2_intro_js/main.js
@@ -0,0 +1,5 @@
+import "./external.js"
+
+console.log("hola deberia aparecer en la consola desde un archivo externo")
+console.warn("hola deberia aperecer en la consola como una alerta desde un archivo externo")
+console.error("hola deberia aperecer en la consola como un error desde un archivo externo")
