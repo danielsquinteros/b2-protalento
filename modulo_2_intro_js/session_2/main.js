69518a3de49d4565db44747771e5376d5bf244d4
Daniel <danielsquinteros@gmail.com>
2022-09-01 19:36:31 -0400

    intro a js

---
+++ modulo_2_intro_js/session_2/main.js
@@ -0,0 +1,34 @@
+// String
+var nombre = "Felipe"
+var nombreConComillaSimple = 'Daniel'
+
+// Number
+var numero = 34.4
+
+// Booleanos -> Verdadero o Falso
+var isPerson = false
+
+// Null
+var nombreSinDefinir = null
+
+// Undefined
+var animal
+
+// Object
+var persona = {
+    nombre: "Daniel",
+    edad: 27,
+    colorFavorito: "negro"
+}
+
+console.log(typeof(nombre))
+console.log(nombreConComillaSimple)
+console.log(typeof(numero))
+console.log(typeof(isPerson))
+
+console.log(nombreSinDefinir)
+console.log(typeof(animal))
+
+console.log(persona)
+console.log(animal)
+console.log(nombreSinDefinir)
