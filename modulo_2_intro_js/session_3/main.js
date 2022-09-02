290badaae429e6004400523aaa400831d53216b7
Daniel <danielsquinteros@gmail.com>
2022-09-01 21:07:58 -0400

    operadores

---
+++ modulo_2_intro_js/session_3/main.js
@@ -0,0 +1,131 @@
+/* Variable asignada a un valor */
+/* Operadores de asignación y tipos de datos */
+
+/* Number */
+var numero = 0
+var decimal = 3.14
+console.log(numero, "tipo de dato", typeof(numero))
+console.log(decimal,"tipo de dato:" ,typeof(decimal))
+
+/* String */
+var nombre = 'Felipe'
+var numeroTexto = '18'
+console.log(nombre, "tipo de dato", typeof(nombre))
+console.log(numeroTexto,"tipo de dato:" ,typeof(numeroTexto))
+
+/* Boolean */
+var verdadero = true
+var falso = false
+console.log(verdadero, "tipo de dato", typeof(verdadero))
+console.log(falso,"tipo de dato:" ,typeof(falso))
+
+/* Undefined  -> indefinido */
+/* vaso no sabemos todavia si esta vacio */
+/* esta indefinido porque no tiene ningun valor asignado */
+var vaso
+console.log(vaso, "tipo de dato", typeof(vaso))
+
+/* Null o vacio */
+/* sabemos que el vaso esta vacio */
+var vaso = null
+console.log(vaso, "tipo de dato", typeof(vaso))
+
+
+/* Object o Objeto */
+var animal = {
+    nombre: "Perro",
+    color: "Cafe",
+    tieneDueño: false,
+    edad: 5
+}
+console.log('El animal es un:', animal.nombre)
+console.log('El animal tiene color:', animal.color)
+
+/* Operadores matemáticos */
+var numero1 = 2
+var numero2 = 3
+
+var resultadoSuma = numero1 + numero2
+console.log('El resultado del a suma es', resultadoSuma)
+
+var resultadoResta = numero2 - numero1
+console.log('El resultado de la resta es', resultadoResta)
+
+var resultadoMultiplicacion = numero1 * numero2
+console.log('El resultado de la multiplicacion es', resultadoMultiplicacion)
+
+var resultadoDivision = numero1 / numero2
+console.log('El resultado de la division es', resultadoDivision)
+
+var resultadoRestoDivision = numero1 % numero2
+console.log('El resultado del resto de la division es', resultadoRestoDivision)
+
+/* elevado = base ** exponente */
+var resultadoExponencial = numero1 ** numero2
+console.log('El resultado de la exponencial es', resultadoExponencial)
+
+var nombreCompleto
+
+var nombre1 = 'Daniel'
+var apellido1 = 'Quinteros'
+var espacio = ' '
+
+console.log('¿Que tipo de datos es espacio?',typeof(espacio))
+nombreCompleto = nombre1 + espacio + apellido1
+console.log(nombreCompleto)
+
+
+/* Operadores de Incremento y Decremento */
+var numero3 = 10
+var resultado1 = - 1 + numero3
+console.log('Resultado1 es igual a:', resultado1)
+
+/* ---- Decremento */
+/* -- es igual a -1 */
+var resultado2 = --numero3
+console.log('Resultado2',resultado2)
+
+/* ---- Incremento */
+/* ++ es igual a +1 */
+var resultado3 = ++numero3
+console.log('Resultado3',resultado3)
+
+var numero4 = 5
+console.log('resultado negativo', numero4 - 10)
+console.log(--numero4)
+console.log(++numero4)
+var resultado4 = --numero4 + ++numero4
+console.log('Resultado4',resultado4)
+
+/* Operadores relacionales */
+/* Siempre van a devolver un valor boleano */
+/* true, false */
+
+var numero5
+var numero6
+
+numero5 = '5'
+numero6 = 5
+
+resultado6 = numero5 > numero6;
+console.log('resultado6', resultado6)
+
+resultado7 = numero5 < numero6;
+console.log('resultado7', resultado7)
+
+resultado8 = numero5 >= numero6
+console.log('resultado8', resultado8)
+
+/* 10 menor o igual que 5 */
+resultado9 = numero5 <= numero6
+console.log('resultado9', resultado9)
+
+/* numero5 es igual a numero6 */
+/* ==  -> compara el valor */
+/* === -> compara el valor y el tipo de dato */ 
+resultado10 = numero5 === numero6
+console.log('resultado10', resultado10)
+
+/* != son diferentes */
+resultado11 = numero5 != numero6
+console.log('resultado11', resultado11)
